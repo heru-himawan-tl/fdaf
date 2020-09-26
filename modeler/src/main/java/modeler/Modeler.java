6c3a6f02fb58ec523267616b221ec1553ff20f0e
Heru Himawan Tejo Laksono <webmaster@bagusteknik.id>
2020-09-26 07:59:28 +0700

    Routine Code Update Sat 26 Sep 2020 07:59:28 AM WIB

--- modeler/src/main/java/modeler/Modeler.java
+++ modeler/src/main/java/modeler/Modeler.java
@@ -56,14 +56,14 @@ public class Modeler {
     private String templatesDirectory;
     
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_BLACK = "\u001B[30m\u001B[1m";
+    public static final String ANSI_RED = "\u001B[31m\u001B[1m";
+    public static final String ANSI_GREEN = "\u001B[32m\u001B[1m";
+    public static final String ANSI_YELLOW = "\u001B[33m\u001B[1m";
+    public static final String ANSI_BLUE = "\u001B[34m\u001B[1m";
+    public static final String ANSI_PURPLE = "\u001B[35m\u001B[1m";
+    public static final String ANSI_CYAN = "\u001B[36m\u001B[1m";
+    public static final String ANSI_WHITE = "\u001B[37m\u001B[1m";
 
     public Modeler(String[] args) throws Exception {
         currentWorkingDirectory = new File(".").getCanonicalPath();
