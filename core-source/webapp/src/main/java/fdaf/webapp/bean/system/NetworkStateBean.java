a9c8ae53bd5bc57eb227598b529f6bf97dc590c7
Heru Himawan Tejo Laksono <heru.htl.83@gmail.com>
2021-09-25 19:49:37 +0700

    Routine Code Update Sat 25 Sep 2021 07:49:37 PM WIB

---
+++ core-source/webapp/src/main/java/fdaf/webapp/bean/system/NetworkStateBean.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) Heru Himawan Tejo Laksono. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holders nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package fdaf.webapp.bean.system;
+
+import fdaf.webapp.websocket.NetworkStateWS;
+import java.io.FileWriter;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.List;
+import java.util.UUID;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@ApplicationScoped
+@Named
+public class NetworkStateBean implements Serializable {
+
+    private static final List<NetworkStateWS> websockets = new ArrayList<NetworkStateWS>();
+    private static final long serialVersionUID = 1L;
+    private ExecutorService executorService;
+    private String serviceUUID = UUID.randomUUID().toString();
+    private boolean networkChanged;
+    private String newIndexURL;
+    
+    public NetworkStateBean() {
+        // NO-OP
+    }
+    
+    public void setNewIndexURL(String newIndexURL) {
+        this.newIndexURL = newIndexURL;
+    }
+    
+    public String getServiceUUID() {
+        return serviceUUID;
+    }
+    
+    public void notifyChange() {
+        networkChanged = true;
+    }
+
+    public void runService() {
+        if (executorService != null && !executorService.isShutdown()) {
+            return;
+        }
+        executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(new Runnable() {
+            public void run() {
+                while (true) {
+                    boolean continueRun = false;
+                    if (!websockets.isEmpty()) {
+                        for (NetworkStateWS ws : websockets) {
+                            if (ws.isOpen()) {
+                                continueRun = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!continueRun && executorService != null && !executorService.isShutdown()) {
+                        websockets.clear();
+                        executorService.shutdownNow();
+                        return;
+                    }
+                    if (executorService.isShutdown()) {
+                        return;
+                    }
+                    try {
+                        if (networkChanged) {
+                            try {
+                                if (!Files.exists(Paths.get("/tmp/ears-network-settings-command"))) {
+                                    networkChanged = false;
+                                }
+                            } catch (Exception ex) {
+                            }
+                            for (NetworkStateWS ws : websockets) {
+                                if (ws.isOpen()) {
+                                    if (networkChanged) {
+                                        ws.sendText(serviceUUID + "," + newIndexURL);
+                                    } else {
+                                        ws.sendText(serviceUUID);
+                                    }
+                                }
+                            }
+                        }
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        return;
+                    }
+                }
+            }
+        });
+    }
+    
+    public void addWebSocket(NetworkStateWS websocket) {
+        if (websocket != null && websocket.isOpen()) {
+            websockets.add(websocket);
+        }
+    }
+}
